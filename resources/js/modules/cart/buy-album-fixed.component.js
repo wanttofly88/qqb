4787633e5d32c72c51c5b8ff7429097fb413c40d
wanttofly88 <wanttofly88@gmail.com>
2017-04-26 22:13:31 +0300

    1.2

--- resources/js/modules/cart/buy-album-fixed.component.js
+++ resources/js/modules/cart/buy-album-fixed.component.js
@@ -1,48 +1,51 @@
 define([
-	'dispatcher'
+	'dispatcher',
+	'slide-scroll/slide-scroll.store',
+	'cart/cart.store'
 ], function(
-	dispatcher
+	dispatcher,
+	slideStore,
+	cartStore
 ) {
 	"use strict";
 
 	var elementProto = Object.create(HTMLButtonElement.prototype);
 
 	elementProto.handleClick = function() {
+		var index;
+		var storeItems = slideStore.getData().items;
+		var dataElement;
+		var id;
+		var productId;
+		var cartData;
+		var found = false;
 		var self = this;
 
-		if (!this._active || !this._productId) return;
+		if (!storeItems.hasOwnProperty(this._parentId)) return;
 
 		this.classList.add('show-cart');
 		setTimeout(function() {
 			self.classList.remove('show-cart');
 		}, 2000);
 
+		index = storeItems[this._parentId].index;
+
+		dataElement = this._sections[index].querySelector('button[is="buy-album"]');
+		productId = dataElement.getAttribute('data-productId');
 
 		dispatcher.dispatch({
 			type: 'cart-add',
-			id: this._productId
+			id: productId
 		});
-	}
-	elementProto.handleSizes = function() {
-		var productId;
-
-		this._productId = this._sizes.getSelected();
-
-		if (this._productId) {
-			this._active = true;
-			this.classList.remove('disabled');
-		} else {
-			this._active = false;
-			this.classList.add('disabled');
-		}
 	}
 
 	elementProto.createdCallback = function() {
-		this._productId = null;
 		this.handleClick = this.handleClick.bind(this);
-		this.handleSizes = this.handleSizes.bind(this);
 	}
 	elementProto.attachedCallback = function() {
+		var parent  = document.getElementsByTagName('slide-scroll')[0];
+		var sections = parent.getElementsByTagName('section');
+
 		var span = document.createElement('span');
 
 		span.innerHTML = this.innerHTML;
@@ -56,19 +59,18 @@ define([
 		span.className = 'added';
 		this.appendChild(span);
 
-		this._productId = this.getAttribute('data-productId');
-		this._sizes = this.parentNode.getElementsByTagName('merch-sizes')[0];
+
+		this._parentId = parent.getAttribute('data-id');
+		this._sections = sections;
 
 		this.addEventListener('click', this.handleClick);
-		this._sizes.addEventListener('size-select', this.handleSizes);
 	}
 
 	elementProto.detachedCallback = function() {
 		this.removeEventListener('click', this.handleClick);
-		this._sizes.removeEventListener('size-select', this.handleSizes);
 	}
 
-	document.registerElement('buy-merch-btn', {
+	document.registerElement('buy-album-fixed', {
 		extends: 'button',
 		prototype: elementProto
 	});
