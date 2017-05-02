e9fd4f4bcc81042a0ddd436c383080beaa6bb731
wanttofly88 <wanttofly88@gmail.com>
2017-05-02 06:20:45 +0300

    1.3

--- resources/js/modules/cart/buy-album.component.js
+++ resources/js/modules/cart/buy-album.component.js
@@ -1,11 +1,17 @@
-define(['dispatcher'], function(dispatcher) {
+define([
+	'dispatcher',
+	'cart/cart.store'
+], function(
+	dispatcher,
+	cartStore
+) {
 	"use strict";
 
 	var elementProto = Object.create(HTMLElement.prototype);
 
 	elementProto.handleClick = function() {
 		var self = this;
-		if (!this._productId) return;
+		if (!this._productId || this._disabled) return;
 
 		this.classList.add('show-cart');
 		setTimeout(function() {
@@ -18,10 +24,33 @@ define(['dispatcher'], function(dispatcher) {
 		});
 	}
 
+	elementProto.handleCart = function() {
+		var items = cartStore.getData().items;
+		var found = false;
+		var self = this;
+
+		items.forEach(function(item) {
+			if (item.id === self._productId) {
+				found = true;
+			}
+		});
+
+		if (found) {
+			this.classList.add('disabled');
+			this._disabled = true;
+		} else {
+			this.classList.remove('disabled');
+			this._disabled = false;
+		}
+	}
+
 	elementProto.createdCallback = function() {
 		this._productId = null;
 		this.handleClick = this.handleClick.bind(this);
+		this.handleCart = this.handleCart.bind(this);
 	}
+
+
 	elementProto.attachedCallback = function() {
 		var span = document.createElement('span');
 
@@ -39,9 +68,13 @@ define(['dispatcher'], function(dispatcher) {
 
 		this._productId = this.getAttribute('data-productId');
 		this.addEventListener('click', this.handleClick);
+
+		this.handleCart();
+		cartStore.eventEmitter.subscribe(this.handleCart);
 	}
 	elementProto.detachedCallback = function() {
 		this.removeEventListener('click', this.handleClick);
+		cartStore.eventEmitter.unsubscribe(this.handleCart);
 	}
 
 	document.registerElement('buy-album', {
